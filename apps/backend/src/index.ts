c121a6b19df307ef0e8b2bf3e68c0ed68f5395ce
h1101241002-ctrl <h1101241002@student.untan.ac.id>
2026-03-29 19:57:52 +0700

    fix API

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -14,6 +14,7 @@ const tokenStore = new Map<
 >();
 
 const app = new Elysia()
+  // !!! modifikasi CORS agar dapat di akses oleh web frontend deployment https
   .use(
     cors({
       origin: process.env.FRONTEND_URL || "http://localhost:5173",
@@ -23,28 +24,6 @@ const app = new Elysia()
   )
   .use(swagger())
   .use(cookie())
-
-  .onRequest(({ request, set }) => {
-    const url = new URL(request.url);
-
-    if (url.pathname.startsWith("/users")) {
-      const origin = request.headers.get("origin");
-      const frontendUrl = process.env.FRONTEND_URL ?? "http://localhost:5173";
-      const key = url.searchParams.get("key");
-
-      // 1. Izinkan jika datang dari Frontend resmi (AJAX/Fetch)
-      if (origin === frontendUrl) {
-        return;
-      }
-
-      // 2. Jika tidak dari Frontend, WAJIB cek API_KEY
-      // Ini akan menangkap akses langsung browser, Postman, cURL, dll.
-      if (key !== process.env.API_KEY) {
-        set.status = 401;
-        return { message: "Unauthorized: Access denied without valid API Key" };
-      }
-    }
-  })
 
   // Health check
   .get(
@@ -72,6 +51,32 @@ const app = new Elysia()
     const oauth2Client = createOAuthClient();
     const url = getAuthUrl(oauth2Client);
     return redirect(url);
+  })
+
+  // !!! tambahkan onRequest ini untuk beri pengamanan API_KEY data `/users`
+  .onRequest(({ request, set }) => {
+    const url = new URL(request.url);
+
+    if (url.pathname.startsWith("/users")) {
+      const origin = request.headers.get("origin");
+      const frontendUrl = process.env.FRONTEND_URL ?? "http://localhost:5173";
+      const keyFromQuery = url.searchParams.get("key");
+      const keyFromHeader = request.headers.get("x-api-key");
+
+      const key = keyFromQuery || keyFromHeader;
+
+      // 1. Izinkan jika datang dari Frontend resmi (AJAX/Fetch)
+      if (origin === frontendUrl) {
+        return;
+      }
+
+      // 2. Jika tidak dari Frontend, WAJIB cek API_KEY
+      // Ini akan menangkap akses langsung browser, Postman, cURL, dll.
+      if (key !== process.env.API_KEY) {
+        set.status = 401;
+        return { message: "Unauthorized: Access denied without valid API Key" };
+      }
+    }
   })
 
   // Google callback setelah login
@@ -181,6 +186,7 @@ const app = new Elysia()
     },
   );
 
+// !!! tambahkan console log yang tidak tampil di production & pakai nilai dari ENV
 if (process.env.NODE_ENV != "production") {
   app.listen(3000);
   console.log(`🦊 Backend → http://localhost:3000`);
@@ -189,5 +195,6 @@ if (process.env.NODE_ENV != "production") {
   console.log(`🦊 GOOGLE_REDIRECT_URI: ${process.env.GOOGLE_REDIRECT_URI}`); // dari file .env
 }
 
-export type App = typeof app;
+// !!! tambahkan export app agar Elysia dapat dibaca Vercel serverless.
 export default app;
+export type App = typeof app;
